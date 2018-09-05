4d94f28057715e2c9c3cf71b46d8e3d5f9e700a9
Chaselyn Isabella <chaselynisabella@gmail.com>
2018-09-04 21:18:37 -0400

    I put my person here

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string
  *
- * @author Bob
+ * @author Chaselyn
  * @version 1.1
  */
 public class Person2 {
@@ -51,4 +51,5 @@ public class Person2 {
 	public String toString(String input) {
 	  return name + calc(input);
 	}
+
 }
